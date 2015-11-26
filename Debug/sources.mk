f37d5f25a0f325b07afb439286574d9ea4309cad
niklaus <niklaus.lee@gmail.com>
2015-11-26 17:23:10 +0900

    replaced nrf51 by nrf52 sdk

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -17,26 +17,25 @@ S_UPPER_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-nrf51_sdk/components/ble/ble_advertising \
-nrf51_sdk/components/ble/ble_db_discovery \
-nrf51_sdk/components/ble/common \
-nrf51_sdk/components/ble/device_manager \
-nrf51_sdk/components/drivers_nrf/common \
-nrf51_sdk/components/drivers_nrf/gpiote \
-nrf51_sdk/components/drivers_nrf/hal \
-nrf51_sdk/components/drivers_nrf/pstorage \
-nrf51_sdk/components/drivers_nrf/spi_master \
-nrf51_sdk/components/drivers_nrf/uart \
-nrf51_sdk/components/libraries/button \
-nrf51_sdk/components/libraries/fifo \
-nrf51_sdk/components/libraries/gpiote \
-nrf51_sdk/components/libraries/scheduler \
-nrf51_sdk/components/libraries/timer \
-nrf51_sdk/components/libraries/trace \
-nrf51_sdk/components/libraries/uart \
-nrf51_sdk/components/libraries/util \
-nrf51_sdk/components/softdevice/common/softdevice_handler \
-nrf51_sdk/components/toolchain/gcc \
-nrf51_sdk/components/toolchain \
+nrf52_sdk/components/ble/ble_advertising \
+nrf52_sdk/components/ble/ble_db_discovery \
+nrf52_sdk/components/ble/common \
+nrf52_sdk/components/ble/device_manager \
+nrf52_sdk/components/drivers_nrf/common \
+nrf52_sdk/components/drivers_nrf/delay \
+nrf52_sdk/components/drivers_nrf/gpiote \
+nrf52_sdk/components/drivers_nrf/pstorage \
+nrf52_sdk/components/drivers_nrf/spi_master \
+nrf52_sdk/components/drivers_nrf/uart \
+nrf52_sdk/components/libraries/button \
+nrf52_sdk/components/libraries/fifo \
+nrf52_sdk/components/libraries/scheduler \
+nrf52_sdk/components/libraries/timer \
+nrf52_sdk/components/libraries/trace \
+nrf52_sdk/components/libraries/uart \
+nrf52_sdk/components/libraries/util \
+nrf52_sdk/components/softdevice/common/softdevice_handler \
+nrf52_sdk/components/toolchain/gcc \
+nrf52_sdk/components/toolchain \
 src \
 
--- src/display.c
+++ src/display.c
@@ -9,63 +9,39 @@
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
-
 #include "app_util_platform.h"
-#include "spi_master.h"
 #include "nrf_error.h"
-
+#include "nrf_drv_spi.h"
 #include "display.h"
 
-// Color definitions
+#define SPIM0_SCK_PIN   29  // SPI clock GPIO pin number.
+#define SPIM0_MOSI_PIN  25  // SPI Master Out Slave In GPIO pin number.
+#define SPIM0_MISO_PIN  28  // SPI Master In Slave Out GPIO pin number.
+#define SPIM0_SS_PIN    12  // SPI Slave Select GPIO pin number.
 
-#define SER_APP_SPIM0_SCK_PIN       29     // SPI clock GPIO pin number.
-#define SER_APP_SPIM0_MOSI_PIN      25     // SPI Master Out Slave In GPIO pin number
-#define SER_APP_SPIM0_MISO_PIN      28     // SPI Master In Slave Out GPIO pin number
-#define SER_APP_SPIM0_SS_PIN        12     // SPI Slave Select GPIO pin number
-
-
-/* DISABLED CALLBACK for SPI0
-void spi_master_event_handler(spi_master_evt_t spi_master_evt)
-{
-    switch (spi_master_evt.evt_type)
-    {
-        case SPI_MASTER_EVT_TRANSFER_COMPLETED:
-            //Data transmission is ended successful. 'rx_buffer' has data received from SPI slave.
-            transmission_completed = true;
-            break;
-
-        default:
-            //No implementation needed.
-            break;
-    }
-}
-*/
+static const nrf_drv_spi_t m_spi_master_0 = NRF_DRV_SPI_INSTANCE(0);
 
 void spi_master_init(void)
 {
-    //Structure for SPI master configuration, initialized by default values.
-    spi_master_config_t spi_config = SPI_MASTER_INIT_DEFAULT;
-
-    //Configure SPI master.
-    spi_config.SPI_Pin_SCK  = SER_APP_SPIM0_SCK_PIN;
-    spi_config.SPI_Pin_MISO = SER_APP_SPIM0_MISO_PIN;
-    spi_config.SPI_Pin_MOSI = SER_APP_SPIM0_MOSI_PIN;
-    spi_config.SPI_Pin_SS   = SER_APP_SPIM0_SS_PIN;
-
-    spi_config.SPI_Freq = SPI_FREQUENCY_FREQUENCY_M2;
-    spi_config.SPI_CONFIG_ORDER = SPI_CONFIG_ORDER_MsbFirst;
-    spi_config.SPI_CONFIG_CPOL = SPI_CONFIG_CPOL_ActiveLow;
-    spi_config.SPI_CONFIG_CPHA = SPI_CONFIG_CPHA_Trailing;
+    nrf_drv_spi_config_t config =
+    {
+    	.sck_pin      = SPIM0_SCK_PIN,
+    	.mosi_pin     = SPIM0_MOSI_PIN,
+    	.miso_pin     = SPIM0_MISO_PIN,
+        .ss_pin       = SPIM0_SS_PIN,
+        .irq_priority = APP_IRQ_PRIORITY_LOW,
+        .orc          = 0xCC,
+        .frequency    = NRF_DRV_SPI_FREQ_1M,
+        .mode         = NRF_DRV_SPI_MODE_3,
+        .bit_order    = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+    };
 
     //Initialize SPI master.
-    uint32_t err_code = spi_master_open(SPI_MASTER_0, &spi_config);
+    uint32_t err_code = nrf_drv_spi_init(&m_spi_master_0, &config, NULL);
     if (err_code != NRF_SUCCESS)
     {
-        //Module initialization failed. Take recovery action.
+        // Initialization failed. Take recovery action.
     }
-
-    //Register SPI master event handler.
-    //spi_master_evt_handler_reg(SPI_MASTER_0, spi_master_event_handler);
 }
 
 uint16_t        buf_len = TX_RX_MSG_LENGTH;
@@ -75,18 +51,18 @@ uint8_t         rx_buffer[0];                   //Receive buffer to get data fro
 void writeCommand(uint8_t c) {
     nrf_gpio_pin_write(DC_PIN, 0); // command
     tx_buffer[0] = c;
-    spi_master_send_recv(SPI_MASTER_0, tx_buffer, buf_len, rx_buffer, 0);
+    nrf_drv_spi_transfer(&m_spi_master_0, tx_buffer, buf_len, rx_buffer, 0);
 }
 
 void writeData(uint8_t c) {
     nrf_gpio_pin_write(DC_PIN, 1); // data
     tx_buffer[0] = c;
-    spi_master_send_recv(SPI_MASTER_0, tx_buffer, buf_len, rx_buffer, 0);
+    nrf_drv_spi_transfer(&m_spi_master_0, tx_buffer, buf_len, rx_buffer, 0);
 }
 
 void spiWrite(uint8_t c) {
     tx_buffer[0] = c;
-    spi_master_send_recv(SPI_MASTER_0, tx_buffer, buf_len, rx_buffer, 0);
+    nrf_drv_spi_transfer(&m_spi_master_0, tx_buffer, buf_len, rx_buffer, 0);
 }
 
 
@@ -170,7 +146,7 @@ void putPixel(uint8_t x, uint8_t y, uint16_t c) {
       pix_buffer[i*2] = c >> 8;
       pix_buffer[i*2+1] = c;
     }
-    spi_master_send_recv(SPI_MASTER_0, pix_buffer, 18, rx_buffer, 0);
+    nrf_drv_spi_transfer(&m_spi_master_0, pix_buffer, 18, rx_buffer, 0);
 }
 
 void drawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint16_t color) {
@@ -186,7 +162,7 @@ void drawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint16_t colo
     for (uint16_t i=0; i < len; i++) {
       tx_buffer[0] = color >> 8;
       tx_buffer[1] = color;
-      spi_master_send_recv(SPI_MASTER_0, tx_buffer, 2, rx_buffer, 0);
+      nrf_drv_spi_transfer(&m_spi_master_0, tx_buffer, 2, rx_buffer, 0);
 
     }
 }
